542515364dab59d5004640849d7f57058fe8466d
broskoff <broskoff@gmail.com>
2026-02-20 13:40:16 +0700

    Создан CalculationPresenter

---
+++ delivery-gulevich-shift-2026/Calculation/CalculationPresenter.swift
@@ -0,0 +1,20 @@
+import Foundation
+import UIKit
+
+protocol ICalculationPresenter: AnyObject {
+	
+	func didTapCalculateButton()
+}
+
+class CalculationPresenter: ICalculationPresenter {
+	
+	weak var coordinator: IMainCoordinator?
+	
+	init(coordinator: IMainCoordinator?) {
+		self.coordinator = coordinator
+	}
+	
+	func didTapCalculateButton() {
+		coordinator?.showMethodOfSend()
+	}
+}
