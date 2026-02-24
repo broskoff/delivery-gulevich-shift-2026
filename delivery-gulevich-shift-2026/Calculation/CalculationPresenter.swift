b1f894b5d7795671826443b0856f9af71038f900
broskoff <broskoff@gmail.com>
2026-02-24 18:08:26 +0700

    Удалена optional зависимость

--- delivery-gulevich-shift-2026/Calculation/CalculationPresenter.swift
+++ delivery-gulevich-shift-2026/Calculation/CalculationPresenter.swift
@@ -10,7 +10,7 @@ class CalculationPresenter: ICalculationPresenter {
 	
 	weak var coordinator: IMainCoordinator?
 	
-	init(coordinator: IMainCoordinator?) {
+	init(coordinator: IMainCoordinator) {
 		self.coordinator = coordinator
 	}
 	
