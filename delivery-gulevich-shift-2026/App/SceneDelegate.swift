4983d11658ab072ab2143eb534fa12d1913c1fa9
broskoff <broskoff@gmail.com>
2026-05-30 11:36:41 +0700

    Рефакторинг: SceneDelegate создает window и передает его в AppCoordinator

--- delivery-gulevich-shift-2026/App/SceneDelegate.swift
+++ delivery-gulevich-shift-2026/App/SceneDelegate.swift
@@ -2,15 +2,16 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-	var appCoordinator: IAppCoordinator?
+	var appCoordinator: CoordinatorProtocol?
 	
 	func scene(_ scene: UIScene,
 			   willConnectTo session: UISceneSession,
 			   options connectionOptions: UIScene.ConnectionOptions) {
 		
 		guard let windowScene = (scene as? UIWindowScene) else { return }
+		let window = UIWindow(windowScene: windowScene)
 		
-		appCoordinator = AppCoordinator(scene: windowScene)
+		appCoordinator = AppCoordinator(window: window)
 		appCoordinator?.start()
 	}
 }
