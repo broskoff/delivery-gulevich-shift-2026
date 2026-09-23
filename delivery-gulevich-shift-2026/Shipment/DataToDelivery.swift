8e8776ef19cecd141cacafe7a914cd7afe86afdd
broskoff <broskoff@gmail.com>
2026-09-23 12:26:10 +0700

    Добавлена модель DataToDelivery
    для накопления пользовательских данных при оформлении доставки

---
+++ delivery-gulevich-shift-2026/Shipment/DataToDelivery.swift
@@ -0,0 +1,26 @@
+struct DataToDelivery {
+	
+	var type: String?
+	var price: String?
+	var days: String?
+	
+	var surnameRecipient: String?
+	var nameRecipient: String?
+	var patronymicRecipient: String?
+	var phoneRecipient: String?
+	
+	var surnameSender: String?
+	var nameSender: String?
+	var patronymicSender: String?
+	var phoneSender: String?
+	
+	var streetSender: String?
+	var houseSender: String?
+	var apartmentSender: String?
+	var noteForСourierSender: String?
+	
+	var streetRecipient: String?
+	var houseRecipient: String?
+	var apartmentRecipient: String?
+	var noteForСourierRecipient: String?
+}
