7d064bf2cb93418a482b8c4920ea6aabf74aa7a3
broskoff <broskoff@gmail.com>
2026-09-23 12:11:38 +0700

    Добавлена реализация RecipientPresenter

--- delivery-gulevich-shift-2026/Shipment/Recipient/RecepientPresenter.swift
+++
@@ -1,7 +0,0 @@
-protocol RecepientPresenterProtocol: AnyObject {
-	
-}
-
-final class RecepientPresenter: RecepientPresenterProtocol {
-	
-}
---
+++ delivery-gulevich-shift-2026/Shipment/Recipient/RecipientPresenter.swift
@@ -0,0 +1,19 @@
+protocol RecipientPresenterProtocol: AnyObject {
+	func userEnteredRecipientData(surname: String, name: String, patronymic: String, phone: String)
+}
+
+final class RecipientPresenter: RecipientPresenterProtocol {
+	
+	weak var output: ShipmentOutputProtocol?
+	weak var view: RecipientViewControllerProtocol?
+	
+	init(output: ShipmentOutputProtocol) {
+		self.output = output
+	}
+	
+	func userEnteredRecipientData(surname: String, name: String, patronymic: String, phone: String) {
+		output?.openSender(surname: surname, name: name, patronymic: patronymic, phone: phone)
+	}
+}
+
+
